0b85c41ae28cbd85b7968e63c297cef4e57f99e7
Mat Moore <MatMoore@users.noreply.github.com>
2025-04-07 16:20:10 +0100

    Correct asset location

--- scripts/copy_nhsuk_frontend.sh
+++ scripts/copy_nhsuk_frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp -r node_modules/nhsuk-frontend/packages/{components,macros,assets} manage_breast_screening/templates/
-
+cp -r node_modules/nhsuk-frontend/packages/{components,macros} manage_breast_screening/templates/
+cp -r node_modules/nhsuk-frontend/packages/assets manage_breast_screening/static/
 cp -r node_modules/nhsuk-frontend/dist/nhsuk-* manage_breast_screening/static/
