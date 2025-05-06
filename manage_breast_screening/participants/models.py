89b6d8c9dee7771bffebb315cb4e12be9e285cf7
Mat Moore <MatMoore@users.noreply.github.com>
2025-05-06 14:40:43 +0100

    Add name, age, extra needs fields to participant

--- manage_breast_screening/participants/models.py
+++ manage_breast_screening/participants/models.py
@@ -1,5 +1,9 @@
+from datetime import date
+
 from django.db import models
+
 from manage_breast_screening.clinics.models import BaseModel
+
 
 class Participant(BaseModel):
     first_name = models.TextField()
@@ -12,3 +16,18 @@ class Participant(BaseModel):
     ethnicity = models.TextField()
     address = models.TextField()
     risk_level = models.TextField()
+    extra_needs = models.JSONField(null=False, default=[])
+
+    @property
+    def full_name(self):
+        return " ".join([name for name in [self.first_name, self.last_name] if name])
+
+    def age(self):
+        today = date.today()
+        if (today.month, today.day) >= (
+            self.date_of_birth.month,
+            self.date_of_birth.day,
+        ):
+            return today.year - self.date_of_birth.year
+        else:
+            return today.year - self.date_of_birth.year - 1
