7d39864d347a6cfa02cf77385d18553e658e36d3
Mat Moore <MatMoore@users.noreply.github.com>
2025-05-16 14:05:10 +0100

    Update to nhsuk-frontend v9.5.2

--- manage_breast_screening/config/jinja2_env.py
+++ manage_breast_screening/config/jinja2_env.py
@@ -77,7 +77,16 @@ def format_age(value: int) -> str:
 def environment(**options):
     env = Environment(**options, extensions=["jinja2.ext.do"])
     if env.loader:
-        env.loader = ChoiceLoader([PackageLoader("nhsuk_frontend_jinja"), env.loader])
+        env.loader = ChoiceLoader(
+            [
+                env.loader,
+                PackageLoader(
+                    "nhsuk_frontend_jinja", package_path="templates/components"
+                ),
+                PackageLoader("nhsuk_frontend_jinja", package_path="templates/macros"),
+                PackageLoader("nhsuk_frontend_jinja"),
+            ]
+        )
 
     env.globals.update(
         {
