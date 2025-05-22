0ed876ecebe537360d8db3d87c4293e4a7c9f99c
Malcolm Baig <git@baig.org>
2025-05-22 12:45:43 +0100

    Update TestAppointmentCannotGoAheadForm test case
    When testing how the reinvite decision is saved:
    
    - Use parametrize to make the test more succinct
    - Assert the starting value of reinvite prior to any changes

--- manage_breast_screening/record_a_mammogram/tests/test_forms.py
+++ manage_breast_screening/record_a_mammogram/tests/test_forms.py
@@ -12,26 +12,17 @@ class TestScreeningAppointmentForm:
 
 @pytest.mark.django_db
 class TestAppointmentCannotGoAheadForm:
-    def test_reinvite_reflects_form_data(self):
+    @pytest.mark.parametrize("decision,reinvite_value", [("True", True), ("False", False)])
+    def test_reinvite_reflects_form_data(self, decision, reinvite_value):
         appointment = AppointmentFactory()
+        assert appointment.reinvite == False
+
         form_data = {
             "stopped_reasons": ["failed_identity_check"],
-            "decision": "True",
+            "decision": decision,
         }
         form = AppointmentCannotGoAheadForm(form_data, instance=appointment)
         form.is_valid()
         form.save()
         appointment.refresh_from_db()
-        assert appointment.reinvite is True
-
-        form_data = {
-            "stopped_reasons": ["failed_identity_check"],
-            "decision": "False",
-        }
-        form = AppointmentCannotGoAheadForm(form_data, instance=appointment)
-        form.is_valid()
-        form.save()
-        appointment.refresh_from_db()
-        assert appointment.reinvite is False
-
-
+        assert appointment.reinvite == reinvite_value
