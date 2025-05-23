a8faf513e24609da159545e336d15ca575590a3a
Mat Moore <MatMoore@users.noreply.github.com>
2025-05-23 10:49:15 +0100

    Correctly link errors to the matching form field
    We have a pattern for presenting errors at the top of the page. When you
    click on the error, the relevant field should be focused (rather than
    the previous behaviour of just navigating to the error)
    
    In the case of checkboxes and radios, we need to link to the first
    option. Since the ErrorList object we are iterating over doesn't know
    whether the field contains checkboxes/radios, the easiest way to do this
    is to always link to `ErrorList.field_id`, which is the ID generated by
    `field.auto_id`, without a suffix like "-1", "-2" etc.
    
    To make this all work, we have to explcitly set the `id` param of the
    first checkbox/radio to the unsuffixed `field.auto_id`, while letting
    the component generate the rest of the IDs with suffixes. If
    https://github.com/nhsuk/nhsuk-frontend/pull/1112 is merged, then we
    will no longer need to specify this, we can just provide the `idPrefix`

--- manage_breast_screening/record_a_mammogram/tests/system/test_recording_a_mammogram.py
+++ manage_breast_screening/record_a_mammogram/tests/system/test_recording_a_mammogram.py
@@ -20,6 +20,9 @@ class TestRecordingAMammogram(SystemTestCase):
         self.appointment = AppointmentFactory(screening_episode=self.screening_episode)
 
     def test_recording_a_mammogram_without_capturing_medical_information(self):
+        """
+        I can record a mammogram without entering any relevant medical information.
+        """
         self.given_i_am_on_the_start_screening_page()
         self.then_i_should_see_the_demographic_banner()
         self.and_i_should_see_the_participant_details()
@@ -28,8 +31,29 @@ class TestRecordingAMammogram(SystemTestCase):
         self.then_i_should_be_on_the_medical_information_page()
         self.and_i_should_be_prompted_to_ask_about_relevant_medical_information()
 
-        self.when_the_participant_shares_no_relevant_medical_information()
+        self.when_i_mark_that_the_participant_shared_no_medical_information()
         self.then_the_screen_should_show_that_it_is_awaiting_images_from_the_PACS()
+
+    def test_filling_out_forms_incorrectly(self):
+        """
+        At each step in the flow, when I fill out the forms incorrectly,
+        then I should see the errors so I can fix them.
+        """
+        self.given_i_am_on_the_start_screening_page()
+        self.when_i_submit_the_form()
+        self.then_i_am_prompted_to_answer_can_the_screening_go_ahead()
+
+        self.when_i_check_the_participants_identity_and_confirm_the_last_mammogram_date()
+        self.then_i_should_be_on_the_medical_information_page()
+
+        self.when_i_submit_the_form()
+        self.then_i_am_prompted_to_answer_has_the_participant_shared_medical_info()
+
+        self.when_i_mark_that_the_participant_shared_medical_information()
+        self.then_i_should_be_on_the_record_medical_information_page()
+
+        self.when_i_submit_the_form()
+        self.then_i_am_prompted_to_confirm_whether_imaging_can_go_ahead()
 
     def given_i_am_on_the_start_screening_page(self):
         self.page.goto(
@@ -54,9 +78,19 @@ class TestRecordingAMammogram(SystemTestCase):
         self.page.get_by_label("Yes, go to medical information").check()
         self.page.get_by_role("button", name="Continue").click()
 
+    def when_i_submit_the_form(self):
+        self.page.get_by_role("button", name="Continue").click()
+
     def then_i_should_be_on_the_medical_information_page(self):
         path = reverse(
             "record_a_mammogram:ask_for_medical_information",
+            kwargs={"id": self.appointment.pk},
+        )
+        expect(self.page).to_have_url(re.compile(path))
+
+    def then_i_should_be_on_the_record_medical_information_page(self):
+        path = reverse(
+            "record_a_mammogram:record_medical_information",
             kwargs={"id": self.appointment.pk},
         )
         expect(self.page).to_have_url(re.compile(path))
@@ -68,8 +102,12 @@ class TestRecordingAMammogram(SystemTestCase):
             )
         ).to_be_visible()
 
-    def when_the_participant_shares_no_relevant_medical_information(self):
+    def when_i_mark_that_the_participant_shared_no_medical_information(self):
         self.page.get_by_label("No - proceed to imaging").check()
+        self.page.get_by_role("button", name="Continue").click()
+
+    def when_i_mark_that_the_participant_shared_medical_information(self):
+        self.page.get_by_label("Yes").check()
         self.page.get_by_role("button", name="Continue").click()
 
     def then_the_screen_should_show_that_it_is_awaiting_images_from_the_PACS(self):
@@ -78,3 +116,24 @@ class TestRecordingAMammogram(SystemTestCase):
             kwargs={"id": self.appointment.pk},
         )
         expect(self.page).to_have_url(re.compile(path))
+
+    def then_i_am_prompted_to_answer_can_the_screening_go_ahead(self):
+        self.expect_validation_error(
+            error_text="This field is required.",
+            fieldset_legend="Can the appointment go ahead?",
+            field_label="Yes, go to medical information",
+        )
+
+    def then_i_am_prompted_to_answer_has_the_participant_shared_medical_info(self):
+        self.expect_validation_error(
+            error_text="This field is required.",
+            fieldset_legend="Has the participant shared any relevant medical information?",
+            field_label="Yes",
+        )
+
+    def then_i_am_prompted_to_confirm_whether_imaging_can_go_ahead(self):
+        self.expect_validation_error(
+            error_text="This field is required.",
+            fieldset_legend="Can imaging go ahead?",
+            field_label="Yes, mark incomplete sections as ‘none’ or ‘no’",
+        )
